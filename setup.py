1a9c5b80162f8c3e0cee0c994c251d9022a7da13
ijnek <kenjibrameld@gmail.com>
2024-04-14 14:09:39 +0000

    Rebase from 'release/rolling/soccer_vision_3d_rviz_markers'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = 'soccer_vision_3d_rviz_markers'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='1.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,6 +15,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
         (os.path.join('share', package_name, 'blender'), glob('blender/*.dae')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
